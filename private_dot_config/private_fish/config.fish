facb033ecc91f86ad09ebcd8a72af9095ce8170b
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2021-04-02 17:53:17 +0200

    removed fnm

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -40,17 +40,6 @@ alias gs='git status'
 
 alias git-clean="git remote prune origin; git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d"
 
-function _fnm_autoload_hook --on-variable PWD --description 'Change Node version on directory change'
-    status --is-command-substitution; and return
-    if test -f .node-version
-        echo "fnm: Found .node-version"
-        fnm use
-    else if test -f .nvmrc
-        echo "fnm: Found .nvmrc"
-        fnm use
-    end
-end
-
 # tmux
 function fs -d "Switch tmux session"
     tmux list-sessions -F "#{session_name}" | fzf | read -l result; and tmux switch-client -t "$result"
@@ -65,8 +54,3 @@ set -gx GOPATH ~/data/programming/go
 set -gx PATH $PATH $GOPATH/bin
 set -gx PATH $PATH $HOME/data/applications
 set -gx PATH $PATH $HOME/bin
-
-# fnm
-fnm env | source
-set -gx NODE_PATH (npm root -g)
-
