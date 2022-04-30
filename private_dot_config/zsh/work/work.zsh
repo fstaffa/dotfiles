83aec8d84e9c89870fab6b8713d03144bc4b63a1
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2022-04-30 20:17:07 +0200

    zsh cleanup and improvements

--- private_dot_config/zsh/work/work.zsh
+++ private_dot_config/zsh/work/work.zsh
@@ -16,7 +16,7 @@ function aws-sapidus {
     _stskeygen_helper sapidus
 }
 
-AWS_PROFILE=logisticsquotingplanning
+AWS_PROFILE=logisticsquotingplanning@admin
 
 function bastion_key {
     SSH_KEY_FILE=~/.ssh/bastion_connect
@@ -25,4 +25,10 @@ function bastion_key {
     instance_id=`aws ec2 describe-instances --filter Name=tag:Name,Values=Bastion Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].{Instance:InstanceId}' --output text`
     echo $instance_id
     aws ec2-instance-connect send-ssh-public-key --region eu-west-1 --instance-os-user ec2-user --ssh-public-key file://$SSH_KEY_FILE.pub --availability-zone eu-west-1a --instance-id $instance_id
+}
+
+function set_ccm_test_keys {
+    E2E_SECRET=$(aws secretsmanager get-secret-value --profile logisticsquotingplanning --secret-id test/ccm/e2e-client | jq ".SecretString | fromjson")
+    export E2E_CLIENT_ID=$(echo $E2E_SECRET | jq -r '.id')
+    export E2E_CLIENT_SECRET=$(echo $E2E_SECRET | jq -r '.secret')
 }
